c352e78e2e2a59208635764b9227b7076708a525
Carlos Santos <carlosrsantos2001@gmail.com>
2024-03-01 01:22:55 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpay/picpay/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.picpay.picpay.repositories;
+
+import com.picpay.picpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+}
